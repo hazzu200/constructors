b87e903304c280923ec95e1a4394e35aeff6210f
ChaitanyaHazarika <chaitanyahazarika9@gmail.com>
2024-04-25 12:32:31 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Human human1  = new Human("Bro", 65, 70);
+        Human human2 = new Human("Chaitanya", 14, 65.7);
+        
+        
+        human2.eat();
+        human1.drink();
+    }
+}
